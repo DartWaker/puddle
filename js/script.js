5f3b26b1cc1991a875c7a857a4b0069aea232669
DartWaker <democracy.potato@gmail.com>
2025-01-02 13:43:32 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-document.querySelector('.header__menu-gamename').addEventListener('click', () => {
+document.querySelector('.header__menu-gamename').addEventListener('click', (event) => {
+  event.stopPropagation(); // Запобігає закриттю меню при кліку по ньому
   const gameList = document.querySelector('.header__gamelist');
   gameList.classList.toggle('active');
 });
+
+document.addEventListener('click', (event) => {
+  const gameList = document.querySelector('.header__gamelist');
+  if (!gameList.contains(event.target) && !event.target.closest('.header__menu-gamename')) {
+    gameList.classList.remove('active');
+  }
+});
+
